e66c4760e5b9ef1852dce6506466794272a67010
Aucons94 <148054502+Aucons94@users.noreply.github.com>
2024-02-15 16:47:28 +0100

    Initial commit

---
+++ VenditaAuto/VenditaAuto/Home.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="VenditaAuto.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Scegli la tua auto</h2>
+            <asp:DropDownList ID="ListaAuto" runat="server"></asp:DropDownList>
+            <h2>Scegli i tuoi Optional</h2>
+            <asp:CheckBoxList ID="ListaOptional" runat="server"></asp:CheckBoxList>
+        </div>
+    </form>
+</body>
+</html>
---
+++ VenditaAuto/VenditaAuto/Home.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VenditaAuto
+{
+    public partial class Home : System.Web.UI.Page
+    {
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                InserisciAuto();
+                InserimentoOptional();
+            }
+
+        }
+        protected void InserisciAuto()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Auto"].ConnectionString.ToString();
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            string IDAuto = Request.QueryString["IDAuto"];
+
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "Select * From Automobili";
+                SqlDataReader reader = cmd.ExecuteReader();
+                ListaAuto.Items.Add(new ListItem("Seleziona un'auto"));
+
+                while (reader.Read())
+                {
+                    string AutoLista = reader["Marca"].ToString() + " " + reader["Modello"].ToString() + " Cilindrata " + reader["Cilindrata"].ToString() + " Elettrica " + reader["Elettrica"].ToString() + " Prezzo " + reader["Prezzo"].ToString();
+                    ListItem li = new ListItem(AutoLista);
+                    ListaAuto.Items.Add(li);
+                }
+
+            }
+            catch
+            {
+                Response.Write("Errore");
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+        protected void InserimentoOptional()
+        {
+            string Optional = ConfigurationManager.ConnectionStrings["Auto"].ConnectionString.ToString();
+            SqlConnection conn = new SqlConnection(Optional);
+           
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "Select * From Optional";
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string OptionalLista = reader["Nome"].ToString() + " Euro " + reader["Prezzo"].ToString();
+                    ListItem li = new ListItem(OptionalLista);
+                    ListaOptional.Items.Add(li);
+
+
+                }
+
+            }
+            catch
+            {
+                Response.Write("Errore");
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
